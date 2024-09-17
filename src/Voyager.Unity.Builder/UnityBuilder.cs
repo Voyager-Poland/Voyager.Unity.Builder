e30d95dc31477f8dacd79f6c4d78139394cdec36
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2024-09-17 23:25:32 +0200

    Factory

--- src/Voyager.Unity.Builder/UnityBuilder.cs
+++ src/Voyager.Unity.Builder/UnityBuilder.cs
@@ -4,17 +4,43 @@ using Unity;
 
 namespace Voyager.Unity.Builder
 {
-	public partial class UnityBuilder : Microsoft.Extensions.DependencyInjection.IServiceProviderFactory<IUnityContainer>
+	public class UnityBuilder : Microsoft.Extensions.DependencyInjection.IServiceProviderFactory<IUnityContainer>
 	{
 		public IUnityContainer CreateBuilder(IServiceCollection services)
 		{
-			UnityContainer unity = new UnityContainer();
+			var unity = new UnityContainer().AddExtension(new Diagnostic());
+			if (services != null)
+			{
+				foreach (var description in services)
+				{
+					if (description.Lifetime == ServiceLifetime.Transient)
+					{
+						if (description.ImplementationFactory != null)
+						{
+							MyCallFactory myCallFactory = new MyCallFactory(unity, this, description.ImplementationFactory);
+							unity.RegisterFactory(description.ServiceType, myCallFactory.Call);
+						}
+						else
+							unity.RegisterType(description.ServiceType, description.ImplementationType);
+					}
+
+				}
+
+			}
 			return unity;
+		}
+
+		T CallServiceFactory<T>(IUnityContainer unityContainer, Func<IServiceProvider, T> funcjaDoWywolania)
+		{
+			var serviceProvider = this.CreateServiceProvider(unityContainer);
+			return funcjaDoWywolania(serviceProvider);
 		}
 
 		public IServiceProvider CreateServiceProvider(IUnityContainer containerBuilder)
 		{
 			return new MyServiceProvider(containerBuilder);
 		}
+
+
 	}
 }
--- test/Voyager.Unity.Builder.Test/UnityBuilderTest.cs
+++ test/Voyager.Unity.Builder.Test/UnityBuilderTest.cs
@@ -1,23 +1,83 @@
 
+using Microsoft.Extensions.DependencyInjection;
+using Unity;
+
 namespace Voyager.Unity.Builder.Test
 {
+
 	public class Tests
 	{
 		private UnityBuilder service;
+		private ServiceCollection serviceDescriptors;
 
 		[SetUp]
 		public void Setup()
 		{
 			service = new Voyager.Unity.Builder.UnityBuilder();
+			serviceDescriptors = new ServiceCollection();
 		}
+
+		[Test]
+		public void NullServicCollection()
+		{
+			var result = PrepareUnit(null!);
+		}
+
 
 		[Test]
 		public void EmptyServicCollection()
 		{
-			var unity = service.CreateBuilder(null);
-			Assert.That(unity, Is.Not.Null);
-			var provider = service.CreateServiceProvider(unity);
-			Assert.That(provider, Is.Not.Null);
+			this.PrepareUnit(serviceDescriptors);
 		}
+
+		[Test]
+		public void TransistientType()
+		{
+			serviceDescriptors.AddTransient<ExampleClass>();
+			var result = PrepareUnit(serviceDescriptors);
+			TestType<ExampleClass>(result);
+		}
+
+		[Test]
+		public void TransistientRegisterFactory()
+		{
+			serviceDescriptors.AddTransient<IExampleInterface>(sp =>
+			{
+				return new ExampleClass();
+			});
+			var result = PrepareUnit(serviceDescriptors);
+			TestType<IExampleInterface>(result);
+		}
+
+
+		[Test]
+		public void TransistientKeyType()
+		{
+			serviceDescriptors.AddTransient<IExampleInterface, ExampleClass>();
+			var result = PrepareUnit(serviceDescriptors);
+			TestType<IExampleInterface>(result);
+		}
+
+		private static void TestType<T>(TestResult result)
+		{
+			var obj1 = result.Unity.Resolve<T>(); ;
+			Assert.That(obj1, Is.Not.Null, $"Nie działa unity {typeof(T)}");
+			var obj2 = result.ServiceProvicer.GetService<T>();
+			Assert.That(obj1, Is.Not.Null, $"Nie działa serviceProver {typeof(T)}");
+		}
+
+		private TestResult PrepareUnit(IServiceCollection serviceDescriptors)
+		{
+			TestResult testResut = new TestResult(this.service, serviceDescriptors);
+			Assert.That(testResut.Unity, Is.Not.Null);
+			Assert.That(testResut.ServiceProvicer, Is.Not.Null);
+			return testResut;
+		}
+
+	}
+
+	public class ExampleClass : IExampleInterface
+	{
+
 	}
 }
