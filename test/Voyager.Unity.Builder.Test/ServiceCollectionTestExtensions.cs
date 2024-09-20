382424f02a1b2340c677812546d6d072a3c64a80
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2024-09-20 15:42:12 +0200

    Rename

--- test/Voyager.Unity.Builder.Test/ServiceCollectionTestExtensions.cs
+++ test/Voyager.Unity.Builder.Test/ServiceCollectionTestExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 namespace Voyager.Unity.Builder.Test
 {
-	public static class ServiceCollectionExtensions
+	static class ServiceCollectionTestExtensions
 	{
 		// Metoda do znajdywania ServiceDescriptor w ServiceCollection na potrzeby testów
 		public static ServiceDescriptor GetServiceDescriptorForType(this IServiceCollection services, Type serviceType)
